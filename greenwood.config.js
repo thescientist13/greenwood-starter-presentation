d58d4399863db1e617e3d5ef57861c83d7eaa9cf
Owen Buckley <owenbuckley13@gmail.com>
2021-08-01 14:37:12 -0400

    share custom properties with custom elements

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,9 +1,11 @@
 const pluginGraphQL = require('@greenwood/plugin-graphql');
+const pluginImportCss = require('@greenwood/plugin-import-css');
 
 module.exports = {
   title: 'My Presentation',
   
   plugins: [
+    ...pluginImportCss(),
     ...pluginGraphQL()
   ]
 };
