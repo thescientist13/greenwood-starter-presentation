667cb9d7c9cc757971397ec99a0edbf0d8dbc81a
Owen Buckley <owenbuckley13@gmail.com>
2021-08-10 16:41:15 -0400

    latest greenwood version and update local deevelopment flow to match Greenwood docs

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,11 +1,34 @@
+const packageName = require('./package.json').name;
+const path = require('path');
 const pluginGraphQL = require('@greenwood/plugin-graphql');
 const pluginImportCss = require('@greenwood/plugin-import-css');
+const { ResourceInterface } = require('@greenwood/cli/src/lib/resource-interface');
+
+class MyThemePackDevelopmentResource extends ResourceInterface {
+  constructor(compilation, options) {
+    super(compilation, options);
+    this.extensions = ['*'];
+  }
+
+  async shouldResolve(url) {
+    return Promise.resolve(url.indexOf(`/node_modules/${packageName}/`) >= 0);
+  }
+
+  async resolve(url) {
+    return Promise.resolve(this.getBareUrlPath(url).replace(`/node_modules/${packageName}/dist/`, path.join(process.cwd(), '/src/')));
+  }
+}
 
 module.exports = {
   title: 'My Presentation',
   
   plugins: [
     ...pluginImportCss(),
-    ...pluginGraphQL()
+    ...pluginGraphQL(),
+    {
+      type: 'resource',
+      name: 'my-theme-pack:resource',
+      provider: (compilation, options) => new MyThemePackDevelopmentResource(compilation, options)
+    }
   ]
 };
